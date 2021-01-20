ffda6ff87705add2b61e719c1d0203a4b3998db8
Luciano Putignano <luciano@eventbrite.com>
2021-01-20 15:20:40 -0300

    Added SPM integration v10

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@
 import PackageDescription
 
 let package = Package(
+    platforms: [.iOS(.v10)],
     name: "Epic",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
